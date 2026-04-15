5f63ebc6bda90b6484a02189168e28582ede6501
Rylzn. <167949712+FarrellDanendra@users.noreply.github.com>
2026-04-15 13:46:08 +0700

    Update tugas3.sql

--- modul-3/250441100015-FARRELL DANENDRA RAFIF BUSTOMI/tugas3.sql
+++ modul-3/250441100015-FARRELL DANENDRA RAFIF BUSTOMI/tugas3.sql
@@ -1,7 +1,7 @@
 SELECT nama_barang, stok,
 CASE
 WHEN stok = 0 AND kategori = "Aksesoris" THEN "Urgent - Restock Segera"
-WHEN stok < 10 AND stok < 0 AND kategori IN ("Laptop","Smartphone") THEN "High Priority"
+WHEN stok < 10 AND stok > 0 AND kategori IN ("Laptop","Smartphone") THEN "High Priority"
 WHEN stok >= 10 AND stok <= 50 THEN "Medium"
 ELSE "Aman"
 END AS prioritas_restock
